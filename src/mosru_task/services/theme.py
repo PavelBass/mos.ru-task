d88e25680543c2168adef30ed2b4b04efc4305b4
Pavel Bass <statgg@gmail.com>
2018-12-28 15:55:22 +0300

    Added SearchService

---
+++ src/mosru_task/services/theme.py
@@ -0,0 +1,37 @@
+from dataclasses import (
+    dataclass,
+    field,
+)
+from typing import List
+
+from mosru_task.enums import ServiceResultStatus
+from mosru_task.mixins import LoggerMixin
+from mosru_task.services.base import ServiceResult
+from mosru_task.tools.theme import ThemesKeeper
+
+
+class SearchService(LoggerMixin):
+    themes_keeper = ThemesKeeper()
+
+    @dataclass
+    class _Data:
+        query: str
+        themes: List[str] = field(default_factory=list)
+
+    async def get_search_result(self, query: str):
+        result = ServiceResult()
+        data = await self._get_search_data(query)
+        if not data.themes:
+            self.logger.info('Query "%s" has no themes', query)
+            result.status = ServiceResultStatus.NoResult
+        result.data = data
+        return result
+
+    async def _get_search_data(self, query) -> _Data:
+        data = self._Data(query=query)
+        if query:
+            data.themes = await self._get_relevant_themes(query)
+        return data
+
+    async def _get_relevant_themes(self, phrase: str) -> List[str]:
+        return await self.themes_keeper.get_relevant_themes(phrase)
